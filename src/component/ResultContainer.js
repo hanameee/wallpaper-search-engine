22fa3da2924ee5e484c293f4cd7ed3cdddf7bc91
hanameee <hanameee@gmail.com>
2022-04-10 18:54:09 +0900

    feat: EmptyResult 컴포넌트 추가

--- src/component/ResultContainer.js
+++ src/component/ResultContainer.js
@@ -4,6 +4,7 @@ import DummyData from '../asset/dummyData';
 import ImageCard from './ImageCard';
 import ImageModal from './ImageModal';
 import Pagination from './Pagination';
+import EmptyResult from './EmptyResult';
 
 const Container = styled.div`
     max-width: 1830px;
@@ -24,12 +25,15 @@ const ResultContainer = () => {
 
     return (
         <Container>
+            {/* ImgCard 클릭 시 해당 이미지의 정보로 ImageModal이 나타나야 합니다. */}
             {/* <ImageModal /> */}
             <Pagination />
             <ResultsWrapper>
                 {data.hits?.map((imgData) => (
                     <ImageCard key={imgData.id} imgData={imgData} />
                 ))}
+                {/* 검색 결과가 없을 시 페이지네이션과 ImgCard 목록 대신 EmptyResult가 렌더되어야 합니다. */}
+                {/* <EmptyResult /> */}
             </ResultsWrapper>
         </Container>
     );
